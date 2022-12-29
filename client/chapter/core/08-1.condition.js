266ac5e3a9c54689f5d71a9af46bb2173eee245f
sm <smyoon22@naver.com>
2022-12-29 11:50:51 +0900

    [추가] if문과 삼항연산자

---
+++ client/chapter/core/08-1.condition.js
@@ -0,0 +1,39 @@
+/* ---------------------------------------------------------------------- */
+/* Condition                                                              */
+/* ---------------------------------------------------------------------- */
+
+let number = prompt('숫자를 입력해주세요',0);
+
+if(number > 0){
+  console.log('1'); 
+} else if(number < 0){
+  console.log('-1');
+} else{
+  console.log('아무것도 아닙니다.');
+}
+
+
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+
+// if 문(statement)
+
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
