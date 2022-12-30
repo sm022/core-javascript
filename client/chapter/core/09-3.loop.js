ca1e04805237f580575aa5faab19c33ea7b085e9
sm <smyoon22@naver.com>
2022-12-30 14:36:13 +0900

    [추가] for 문 학습

---
+++ client/chapter/core/09-3.loop.js
@@ -0,0 +1,68 @@
+/* ---------------------------------------------------------------------- */
+/* For Loop                                                               */
+/* ---------------------------------------------------------------------- */
+
+
+// for(let i = 0; i < 10; i++){
+//   console.log(i)
+// }
+
+// let j = 0;
+// for(;  j < 10; j++){
+//   //console.log(i)
+// }
+
+// console.log(j);
+
+// 2 ~ 10까지의 짝수 출력하기
+
+
+for(let j = 1; j <= 10; j++){
+
+  if( i % 2 !== 0 ) continue;
+
+  // console.log(j);
+}
+
+
+
+
+
+
+
+const frontEndDev = 'HTML CSS SVG JavaScript jQuery React Redux'.split(' ');
+
+// let i = 0;
+// let l = frontEndDev.length;
+
+// while(i < l) {
+//   // console.log(frontEndDev[i]);
+//   i += 1;
+// }
+
+
+//   - 조건이 맞을 경우, 이어서(continue) 순환
+//   - 조건: SVG, jQuery는 출력하지 마세요.
+
+
+let i = 0;
+for(; i < frontEndDev.length ; i++){
+  
+  let result = frontEndDev[i];
+
+  // if(frontEndDev[i].includes('SVG') || result.includes('jQuery')) continue;
+
+  if(result.includes('jQuery')) break;
+
+  console.log(result);
+}
+
+
+// while 문 → for 문 (순환)
+// - 실행 흐름
+// - 순환 중단 또는 이어서 순환
+
+
+
+//   - 무한 루프 (브레이크)
+//   - for 문 (역순환)
