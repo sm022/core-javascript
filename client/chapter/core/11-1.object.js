7eb9f57c5c78077a975f332fc6c1b41942a7f276
sm <smyoon22@naver.com>
2023-01-04 15:38:05 +0900

    [추가] 참조에 의한 복사

--- client/chapter/core/11-1.object.js
+++ client/chapter/core/11-1.object.js
@@ -134,7 +134,26 @@ let result = getPropertiesList(authUser)
 
 // 유틸 함수
 
-removeProperty()
+// 1. 함수 이름 정하기
+// 2. 인수(argumemts)와 인자(parameter) 정하기
+// 3. 확인하기(validation)
+// 4. 리턴값(return)
+
+function removeProperty(object, key){
+  if(!(typeof object === 'object')){
+    // throw
+  }
+  object[key] = null;
+}
+
+
+
+function deleteProperty(object,key){
+  delete object[key]
+}
+
+
+deleteProperty(authUser,'uid')
 
 
 // 단축 프로퍼티
@@ -144,11 +163,82 @@ let authorization = 'Lv. 99';
 let isLogin = true;
 
 
+// shortand property
+const student = { name, email, authorization, isLogin }
+
+
+
 // 프로퍼티 이름 제한
 // 예약어: class, if, switch, for, while, ...
 
 
 // 객체가 프로퍼티를 포함하는 지 유무를 반환하는 유틸리티 함수 isEmptyObject 작성
-function isEmptyObject() {
-  return null;
+// 객체의 키를 배열로 바꾸고 그 배열의 갯수가 0이면 ? true : false
+
+function isEmptyObject(object) {
+ 
+  // if(Object.keys(object).length){
+  //   return true
+  // }else{
+  //   return false
+  // }
+
+  // return Object.keys(object).length === 0 ? true : false
+  return Object.keys(object).length === 0;
 }
+
+const empty = {}
+
+console.log( isEmptyObject(empty) );
+
+
+
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 430 
+}
+
+// 월급의 총 합을 구하세요.
+
+/* 배열 구조 분해 할당 destructuring assingments */
+let total = 0;
+for(let [key, value] of Object.entries(salaries)) {
+  
+  console.log(key);
+  // console.log(keyValue);
+  // let key = keyValue[0];
+  // let value = keyValue[1];
+
+
+  total += value;
+}
+
+console.log(total);
+
+/* ---------------------------------------------------------------------- */
+/* 배열 구조 분해 할당 destructuring assingments                             */
+/* ---------------------------------------------------------------------- */
+
+
+
+let color = ['#ff0000', '#2b00ff', '#00ff2f'];
+
+// const COLOR_RED = color[0];
+// const COLOR_BLUE = color[1];
+// const COLOR_GREEN = color[2];
+// = 밑 식이랑 같음 
+
+const [,,Color_GREEN] = color 
+
+console.log(Color_GREEN);
+
+// 발췌
+
+/* ---------------------------------------------------------------------- */
+/* 객체 구조 분해 할당 destructuring assingments                             */
+/* ---------------------------------------------------------------------- */
+
+
+const { John, Ann, Pete} = salaries;
