587f91c9af81e9bc74c3e3efad23c21fb59d72f1
sm <smyoon22@naver.com>
2023-01-06 11:23:39 +0900

    [추가] reduce 학습

--- client/chapter/core/16.string.js
+++ client/chapter/core/16.string.js
@@ -7,47 +7,89 @@ let message = 'Less is more.';
 
 
 // length 프로퍼티
-let stringTotalLength;
+let stringTotalLength = message.length;
+console.log('stringTotalLength : ',stringTotalLength)
 
 
 // 특정 인덱스의 글자 추출
-let extractCharacter;
+let extractCharacter = message[0];
+console.log('extractCharacter : ',extractCharacter)
+
 
 
 // 문자열 중간 글자를 바꾸는 건 불가능 
 // (기존 문자 변경 대신, 새로운 문자를 생성해야 함)
 let immutableChangeCharacter;
 
+// message[3] = 'more' + message[5];
+// message[3] = 'i'
+
+console.log(message);
+
 
 // 부분 문자열 추출
-let slice;
-let subString;
-let subStr;
+let slice = message.slice(0,3); // (안에 숫자를 넣으면) 특정 부분(인덱스)만 출력
+console.log('slice : ',slice)
+
+let subString = message.substring(0,3);
+console.log('slice : ',slice)
+
+// let subStr;
 
 
 // 문자열 포함 여부 확인
-let indexOf;
-let lastIndexOf;
-let includes;
-let startsWith;
-let endsWith;
+let indexOf = message.indexOf('i');
+console.log('indexOf : ',indexOf)
+
+let lastIndexOf = message.lastIndexOf('i');
+
+let includes = message.includes('Less');
+console.log('includes : ',includes)
+
+let startsWith = message.startsWith('L');
+console.log('startsWith : ',startsWith)
+
+let endsWith = message.endsWith('.');
+console.log('endsWith : ',endsWith)
+
 
 
 // 공백 잘라내기
-let trimLeft;
-let trimRight;
-let trim;
+// let trimLeft = message.trimLeft();
+// console.log('trimLeft : ',trimLeft)
+
+// let trimRight;
+// console.log('trimRight : ',trimRight)
+
+let trim = message.replace(/\s*/g,'');
+console.log('trim : ',trim)
 
 
 // 텍스트 반복
-let repeat;
+let repeat = message.repeat(3);
+console.log('repeat : ',repeat)
 
 
 // 대소문자 변환
-let toLowerCase;
-let toUpperCase;
+let toLowerCase = message.toLowerCase();
+console.log('toLowerCase :',toLowerCase)
+
+let toUpperCase = message.toUpperCase;
+console.log('toUpperCase :',toUpperCase)
 
 
 // 텍스트 이름 변환 유틸리티 함수
-let toCamelCase;
-let toPascalCase;
+// let toCamelCase;
+// let toPascalCase;
+
+function toCamelCase(string) {
+  return string.replace(/(\s|-|_)+./g, ($1) => $1.trim().replace(/(-|_)+/, '').toUpperCase())
+}
+
+function toPascalCase(string) {
+  let name = toCamelCase(string);
+  return name[0].toUpperCase() + name.slice(1);
+}
+
+toCamelCase('less-is-more') // lessIsMore
+toPascalCase('less-is-more') // lessIsMore
---
+++ client/chapter/core/18.arrayMethod.js
@@ -0,0 +1,176 @@
+/* ---------------------------------------------------------------------- */
+/* Array's Methods                                                        */
+/* ---------------------------------------------------------------------- */
+
+// Array.isArray
+const arr = [10,100,1000,10000]
+
+function isArray(data){
+  return Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'array'
+}
+
+function isNull(data){
+  return Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'null'
+}
+
+
+console.log(Array.isArray([]));
+
+
+
+/* 요소 순환 -------------------------------------------------------------- */
+
+// forEach
+const user = {}
+
+arr.forEach(function(item,index){
+  //user[0] = 10
+  this[index] = item
+},user)
+
+
+// span[0].addEventListener('click',()=>{
+//   console.log('first');
+// })
+
+const span = document.querySelectorAll('span');
+
+span.forEach((item)=> {
+
+  item.addEventListener('click',(e)=>{
+    console.log(e.target.style.background = 'orange');
+    // console.log(index);
+  })
+
+})
+
+/* 원형 파괴 -------------------------------------------------------------- */
+
+// push
+// pop
+// unshift
+// shift
+// reverse
+arr.reverse()
+// console.log(arr);
+
+// splice
+//        시작 제거 [추가]
+arr.splice(1,0,23,5)
+// console.log(arr);
+
+//반환 값 < 0 : a가 b보다 앞에 있어야 한다.
+//반환 값 = 0 : a와 b의 순서를 바꾸지 않는다.
+//반환 값 > 0 : b가 a보다 앞에 있어야 한다.
+
+// sort
+arr.sort((a,b)=>{
+  return a - b // b - a
+})
+// console.log(arr);
+
+/* 새로운 배열 반환 --------------------------------------------------------- */
+
+// concat
+// slice
+// map
+
+let todo = ['밥먹기','복습하기','공부하기'];
+
+let template = todo.map((todolist)=>{
+  return /* html */ `<li>${todolist}</li>`
+})
+
+template.forEach((item) => {
+  document.body.insertAdjacentHTML("beforeend", item);
+});
+
+let newArray = arr.map( item => item * 2)
+console.log(newArray);
+
+/* 요소 포함 여부 확인 ------------------------------------------------------ */
+
+// indexOf
+
+// console.log( arr.indexOf(5) );
+
+
+// lastIndexOf
+// includes
+
+// console.log( arr.includes(10) );
+
+/* 요소 찾기 -------------------------------------------------------------- */
+const users = [
+  {id:1,name:'하나'},
+  {id:2,name:'투투'},
+  {id:3,name:'오륙'},
+]
+// find
+
+const find = users.find((item,index)=>{
+  return item.id === 3 // id < 2 , id < 5
+})
+
+// console.log(find)
+
+
+// findIndex
+const findIndex = users.findIndex((item)=>{
+  return item.id === 3 
+})
+
+console.log(findIndex);
+
+/* 요소 걸러내기 ----------------------------------------------------------- */
+
+// filter
+
+let result = arr.filter((number)=>{
+  return number > 100
+})
+
+console.log(result);
+
+/* 요소별 리듀서(reducer) 실행 ---------------------------------------------- */
+
+const friends = [
+  {
+    name: "이하나",
+    age: 26,
+    job: "무직",
+  },
+  {
+    name: "김투투",
+    age: 24,
+    job: "취준생",
+  },
+  {
+    name: "박오륙",
+    age: 21,
+    job: "학생",
+  },
+];
+
+// reduce
+
+// 친구들 나이의 총 합을 구하세요.
+let age = friends.reduce((acc,cur)=>{
+  return acc + cur.age
+},0)
+
+
+let template2 = todo.reduce((acc,cur)=>{
+  return /* html */ acc + `<li>할일 : ${cur}</li>`
+},'')
+
+console.log(template2);
+
+console.log(age);
+
+// reduceRight
+
+/* string ←→ array 변환 ------------------------------------------------- */
+
+// split
+// join
