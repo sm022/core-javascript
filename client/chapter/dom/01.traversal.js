f67ba7eb2002f3324eae8ef2bd9f131980bbb8f2
sm <smyoon22@naver.com>
2023-01-10 13:02:09 +0900

    [추가] attr 학습

--- client/chapter/dom/01.traversal.js
+++ client/chapter/dom/01.traversal.js
@@ -31,7 +31,7 @@
 
 // let first = document.querySelector('.first');
 
-let [first,second] = document.querySelectorAll('span');
+// let [first,second] = document.querySelectorAll('span');
 
 // let first = span[0];
 // let second = span[1];
@@ -68,4 +68,28 @@ function getNodes(node){
 
 /* 문서 대상 확인 */
 // - matches
+//          선택자 안에 class || id를 가지고 있는 대상이 있는지?
+
+console.log(getNode('.first').matches('.first'));
 // - contains
+
+
+//            선택자의 자식들 중에 해당 element가 있는지?
+console.log(getNode('h1').contains(getNode('.first')));
+
+
+
+
+
+
+let first = getNode(".first");
+
+let clicked = false;
+document.addEventListener("click", () => {
+  if (first.classList.contains("first") && !clicked) {
+    first.classList.add("is-active");
+  } else {
+    first.classList.remove("is-active");
+  }
+  clicked = !clicked;
+});
