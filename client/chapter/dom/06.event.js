5f0a13607bb9a29daf0cf761b9b9db82ffae7d4d
sm <smyoon22@naver.com>
2023-01-11 14:09:26 +0900

    [추가] bindevent 함수 만들기

---
+++ client/chapter/dom/06.event.js
@@ -0,0 +1,39 @@
+/* ---------------------------------------------------------------------- */
+/* Event Handling                                                         */
+/* ---------------------------------------------------------------------- */
+
+
+/* 이벤트 핸들링 3가지 방법 --------------------------------------------------- */
+
+// 1. HTML 속성 : onclick="handler()"
+// 2. DOM 프로퍼티 : element.onclick = handler
+// 3. 메서드 : element.addEventListener(event, handler[, phase])
+
+
+/* 이벤트 추가/제거 --------------------------------------------------------- */
+
+
+const first = getNode('.first');
+
+function handler(){
+  console.log('hit !');
+
+  // css('.second','display','none')
+
+}
+
+// first.addEventListener('click',handler);
+
+
+
+
+
+const off = bindEvent('.first','click',handler);
+
+
+bindEvent('.second','click',off);
+
+
+
+// - addEventListener
+// - removeEventListener
