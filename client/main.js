b78fb1eb0569646514596873f9671a3cc1b13b64
sm <smyoon22@naver.com>
2023-01-16 11:20:09 +0900

    [추가] toggle click

--- client/main.js
+++ client/main.js
@@ -12,16 +12,40 @@ import { diceAnimation, getNode } from "./lib/index.js";
 ..diceAnimation()
 */
 
+/*
+[주사위 굴리기]
+1. dice 애니메이션 불러오기
+2. bindEvent 유틸 함수 만들기
+3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
+4. 변수 보호를 위한 클로저 + IIFE 사용하기
+
+..diceAnimation()
+*/
+
 
 const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
 
 
+// IIFE
+const handlerRollingDice = (() => {
+  
+  let isRolling = false;
+  let stopAnimation;
+  // diceAnimation()
+  // stopAnimation = setInterval(diceAnimation,100)
 
-const handlerRollingDice = () => {
-  
-  diceAnimation()
-  
-}
+  return () => {
+    if(!isRolling){
+      stopAnimation = setInterval(diceAnimation,100)
+    }else{
+      clearInterval(stopAnimation);
+   }
+
+  isRolling = !isRolling;
+ }
+
+})()
+
 
 rollingDiceButton.addEventListener('click',handlerRollingDice)
 
