fa2d5aaa1b566ae9121ecedf8d683bdc48eeaf12
sm <smyoon22@naver.com>
2023-01-18 10:28:43 +0900

    [추가] dealyP parameter 설정하기

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -12,6 +12,7 @@ function delay(callback,timeout = 1000){
   setTimeout(callback, timeout);
 }
 
+/*
 delay(()=>{
   first.style.top = '-100px';
   delay(()=>{
@@ -21,3 +22,39 @@ delay(()=>{
       first.style.transform = 'rotate(360deg)';
   })
 })
+*/
+
+
+delayP()
+.then(()=>{
+  first.style.top = '-100px';
+  return delayP()
+})
+.then(()=>{
+  first.style.transform = 'rotate(360deg)';
+  return delayP()
+})
+.then(()=>{
+  first.style.top = '0px';
+})
+
+
+
+function delayP(shouldReject = false, timeout = 1000, data='성공했습니다.', errorMessage = '알 수 없는 오류가 발생했습니다.'){
+
+  return new Promise((resolve, reject)=>{
+    
+    setTimeout(() => {
+      !shouldReject ? resolve(data) : reject(errorMessage);
+    }, timeout);
+  })
+}
+
+
+delayP(false,1000,'성공했습니다.','오류가 발생했습니다.');
+// .then(res=>console.log(res))
+// .catch(err=>console.log(err))
+
+
+
+// async await
